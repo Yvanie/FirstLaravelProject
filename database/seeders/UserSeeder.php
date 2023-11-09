e45fdc8945ad8c949876b7b4e6fc9598f53f5d5b
Nwouatou Yvanie Noelle <nwouatouyvanienoelle@gmail.com>
2023-11-09 17:10:03 +0100

    adding users manually using seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,6 +13,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        //User::factory(5)->create();
+        User::factory()->create([
+                'name' => 'Yvanie Noelle',
+                'email' => 'yva@example.com',
+                'email_verified_at' => '2023-11-09 15:15:29',
+                'password'=>'$2y$12$nkxoEFD/0X53muTHl4TLuO1F2cTnNPHHoeJlgdiLOnHrmahKH32Pm'
+             ]);
+             User::factory()->create([
+                'name' => 'Jean Claude',
+                'email' => 'jean@example.com',
+                'email_verified_at' => '2023-11-09 15:15:29',
+                'password'=>'$2y$12$nkxoEFD/0X53muTHl4TLuO1F2cTnNPHHoeJlgdiLOnHrmahKH32Pm'
+             ]);
+             User::factory()->create([
+                'name' => 'Vad',
+                'email' => 'vad@example.com',
+                'email_verified_at' => '2023-11-09 15:15:29',
+                'password'=>'$2y$12$nkxoEFD/0X53muTHl4TLuO1F2cTnNPHHoeJlgdiLOnHrmahKH32Pm'
+             ]);
+
+
+
     }
 }
